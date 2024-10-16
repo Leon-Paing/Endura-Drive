8e18f788f95726744370b795ce714a21a8c748fe
Leon-Paing <yanpaingphyoe441@gmail.com>
2024-10-16 13:42:31 +0630

    adjusted location,phone and mygarage divs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,11 +12,11 @@ const Navbar = () => {
                     <div className="w-1/6 flex h-full justify-center items-center p-2">
                         <img className="w-full h-full object-cover" src="./Logo.jpg" alt="" />
                     </div>
-                    <div className="w-3/4 xs:w-4/5 h-full flex flex-col justify-end items-end p-0">
+                    <div className="w-4/5 xs:w-4/5 h-full flex flex-col justify-end items-end p-0">
                         <div className="w-full h-2/3 flex justify-end justify-items-end items-center text-white pr-0 gap-2 m-0">
-                            <div className="xl:w-96 xs:w-96 h-full flex text-center justify-start items-center xl:p-3 lg:p-3 md:p-3 xs:p-4 xl:text-base sm:text-sm xs:text-xxxs text-slate-100"><FaLocationDot className="text-red-600 text-base"/> &nbsp;KabarAye Pagoda Road, Yangon, Myanmar</div>
-                            <div className="xl:w-40 xs:w-80 h-full flex text-center justify-center items-center xl:text-base lg:text-base md:text-base sm:text-sm xs:text-xxs"><FaPhone className="text-red-600"/>&nbsp; +95 9776357441</div>
-                            <div className="xl:w-40 md:w-36 sm:w-36 xs:w-52 xs:h-9 xl:h-full lg:h-full md:h-full flex bg-red-600 text-center justify-center items-center cursor-pointer hover:bg-red-500 rounded-sm xl:text-base lg:text-base md:text-base sm:text-xs xs:text-xxs" href="">My Garage ({garage})</div>
+                            <div className="xl:w-96 xs:w-[480px] h-[40px] flex text-center justify-center items-center xl:p-3 lg:p-3 md:p-3 xs:p-4 xl:text-base sm:text-sm xs:text-xxxs text-slate-100"><FaLocationDot className="text-red-600 text-base"/> &nbsp;KabarAye Pagoda Road, Yangon, Myanmar</div>
+                            <div className="xl:w-40 xs:w-[330px] h-full flex text-center justify-center items-center xl:text-base lg:text-base md:text-base sm:text-sm xs:text-xxxs"><FaPhone className="text-red-600"/>&nbsp; +95 9776357441</div>
+                            <div className="xl:w-40 md:w-36 sm:w-36 xs:w-60 xs:h-9 xl:h-full lg:h-full md:h-full flex bg-red-600 text-center justify-center items-center cursor-pointer hover:bg-red-500 rounded-sm xl:text-base lg:text-base md:text-base sm:text-xs xs:text-xxs" href="">My Garage ({garage})</div>
                         </div>
                         <div className="w-4/5 h-2/3 flex justify-end justify-items-end items-center text-white p-1 gap-1">
                             <div className="xl:w-40 lg:w-40 md:w-36 sm:w-16 xs:w-14 xl:h-12 lg:h-10 md:h-9 xs:h-7 flex text-center justify-center items-center border-red-500 border-r-2 cursor-pointer rounded-sm hover:text-slate-300 active:bg-red-600 active:text-white" href="">Stocks</div>
