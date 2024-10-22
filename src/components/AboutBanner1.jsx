71a64b2a57e462d3a75b81262aa6b57a7e58f363
Leon-Paing <yanpaingphyoe441@gmail.com>
2024-10-22 15:14:34 +0630

    increase width of contact us div in about page

--- src/components/AboutBanner1.jsx
+++ src/components/AboutBanner1.jsx
@@ -17,7 +17,7 @@ const AboutBanner1 = () => {
                     <div className="w-full xl:h-48 float-left text-xl tracking-widest font-sans justify-start text-justify">Welcome to Endura <span className="text-red-600">Drive</span>
                     , Myanmar's trusted car dealership for buying, selling, and exchanging both new and used vehicles. Whether you're looking for a reliable pre-owned car or a brand-new model, we've got you covered with a wide range of options to suit every need and budget.</div>
                     <div className="w-full h-36 flex justify-start items-center">
-                        <div className="w-32 flex text-white bg-red-600 p-4 rounded-md cursor-pointer hover:text-slate-300" onClick={handleContactPage}>Contact Us</div>
+                        <div className="w-36 flex text-white bg-red-600 p-4 rounded-md cursor-pointer hover:text-slate-300" onClick={handleContactPage}>Contact Us</div>
                     </div>
                 </div>
                 <div className="xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-full xs:w-full p-0">
