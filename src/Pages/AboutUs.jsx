572064db3acafababcbe56a3c06271fced436749
Leon-Paing <yanpaingphyoe441@gmail.com>
2024-10-22 15:12:35 +0630

    added About Us page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 import AboutBanner1 from "../components/AboutBanner1";
 import AboutBanner2 from "../components/AboutBanner2";
 import AboutBanner3 from "../components/AboutBanner3";
-import AboutHomeLink from "../components/aboutHomeLink";
+import AboutSection from "../components/AboutSection";
 
 
 const AboutUs = () => {
@@ -13,7 +13,7 @@ const AboutUs = () => {
         <>
         <div className="w-screen h-auto justify-center items-center p-0 bg-slate-900">
             <Navbar/>
-            <AboutHomeLink/>
+            <AboutSection/>
             <AboutBanner1/>
             <AboutBanner2/>
             <AboutBanner3/>
--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate,useLocation } from "react-router-dom";
 
-const AboutHomeLink = () => {
+const AboutSection = () => {
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -26,4 +26,4 @@ const AboutHomeLink = () => {
     )
 }
 
-export default AboutHomeLink;
+export default AboutSection;
